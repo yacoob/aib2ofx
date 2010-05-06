d3bb30b4ec75653ca9b004fe3d7503cecaee04bf
Jakub Turski <yacoob@gmail.com>
2010-05-07 00:04:13 +0100

    Initial commit.
    As it stands right now, aib.py can log in to the bank, grab some data.
    The quality of said data isn't that good atm.

---
+++ aib2wesabe/cfg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json, os;
+
+import mechanize;
+
+class config:
+    def __init__(self, config_filename='~/.aib-sucker'):
+        fp = open(os.path.expanduser(config_filename));
+        self.cfg = json.load(fp);
+        fp.close();
+
+    def get_config(self):
+        return self.cfg;
+
+    def __getattr__(self, name):
+        if self.cfg.has_key(name):
+            return self.cfg[name];
+        else:
+            raise AttributeError;
