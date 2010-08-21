7b1b2f1b8fa6eb98c3b09d887eb0367ad8d640e8
Jakub Turski <yacoob@gmail.com>
2010-08-21 01:09:04 +0100

    Simple wrapper script (get all accounts).

---
+++ aib2ofx/aib2ofx.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re;
+
+import aib, cfg, ofx;
+
+def main():
+    cleanup_re = re.compile('[- 	]+');
+    config = cfg.config();
+    formatter = ofx.ofx()
+
+    for user in config.users():
+        creds = config[user]
+        bank = aib.aib(creds);
+        bank.login();
+        bank.scrape();
+        bank.bye();
+
+        for account in bank.getdata().values():
+            name = re.sub(cleanup_re,
+                          '_',
+                          account['accountId']).lower();
+            f = open('/tmp/%s_%s.ofx' % (user, name),
+                     'w');
+            f.write(formatter.prettyprint(account));
+            f.close;
+
+if __name__ == '__main__':
+    main();
