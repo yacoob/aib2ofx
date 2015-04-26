3ff19be1592be1a1b4711e5fc38e3aef10cd1584
Jakub Turski <yacoob@gmail.com>
2015-04-26 21:17:16 +0100

    Update setup.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(name='aib2ofx',
       description='Download data from aib.ie in OFX format',
-      version='0.33',
+      version='0.4',
       author='Jakub Turski',
       author_email='yacoob@gmail.com',
       url='http://github.com/yacoob/aib2ofx',
 
-      install_requires=['BeautifulSoup>=3.2.0', 'mechanize'],
+      install_requires=['BeautifulSoup>=3.2.0', 'mechanize', 'python-dateutil'],
 
       packages=['aib2ofx_lib'],
       scripts=['aib2ofx'],
