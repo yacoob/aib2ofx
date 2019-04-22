1506e2d651e01640b22af9d8e399ffd35870abe8
Jakub Turski <yacoob@gmail.com>
2019-04-22 19:56:58 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
+"""Setup data for aib2ofx."""
 
 from setuptools import setup
 
-setup(name='aib2ofx',
-      description='Download data from aib.ie in OFX format',
-      version='0.5.1',
-      author='Jakub Turski',
-      author_email='yacoob@gmail.com',
-      url='http://github.com/yacoob/aib2ofx',
-      packages=['aib2ofx'],
-      entry_points={
-          'console_scripts': [
-              'aib2ofx = aib2ofx.main:main'
-          ],
-      },
-      install_requires=[
-          'BeautifulSoup>=3.2.0',
-          'mechanize==0.2.5',
-          'python-dateutil'
-      ],
- );
+setup(
+    name='aib2ofx',
+    description='Download data from aib.ie in OFX format',
+    version='0.6',
+    author='Jakub Turski',
+    author_email='yacoob@gmail.com',
+    url='http://github.com/yacoob/aib2ofx',
+    packages=['aib2ofx'],
+    entry_points={
+        'console_scripts': ['aib2ofx = aib2ofx.main:main'],
+    },
+    dependency_links=[
+        'git+https://github.com/MechanicalSoup/MechanicalSoup'
+        '#egg=mechanicalsoup --process-dependency-links'
+    ],
+    install_requires=[
+        'mechanicalsoup',
+    ],
+)
